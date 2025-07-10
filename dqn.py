788d74e9e2b9034735e32f09ce75ab8d76395bd3
Christopher-Guarino1 <christopher.guarino1@ibm.com>
2025-07-10 09:37:50 -0400

    Implimented dueling dqn

--- dqn.py
+++ dqn.py
@@ -5,17 +5,46 @@ import torch.nn.functional as F
 class DQN(nn.Module): 
 
     #This function defines the layers. 
-    def __init__(self, state_dim, action_dim, hidden_dim=256): 
+    def __init__(self, state_dim, action_dim, hidden_dim=256, enable_dueling_dqn = True): 
         super(DQN, self).__init__() 
+
+        self.enable_dueling_dqn = enable_dueling_dqn
 
         #Input layer (fc0) is implicit in PyTorch 
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, action_dim)
+        
+        # Dueling DQN implementation
+        if self.enable_dueling_dqn: 
+            # Value Stream Layers
+            self.fc_value = nn.Linear(hidden_dim, 256)
+            self.value = nn.Linear(256, 1)
+
+            # Advantages Stream Layers 
+            self.fc_advantages = nn.Linear(hidden_dim, 256)
+            self.advantages = nn.Linear(256, action_dim)
+        else:
+            self.fc2 = nn.Linear(hidden_dim, action_dim) # Non-Dueling DQN output layer
 
     #This function does the calculations, transforms
     def forward(self, x):
+        # Neuron Activation Funtions 
         x = F.relu(self.fc1(x)) #x is state, in this case 12 values in an array that represent the current env state
-        return self.fc2(x) 
+
+        # Check for Dueling DQN
+        if self.enable_dueling_dqn: 
+            # Value Calc 
+            v = F.relu(self.fc_value(x))
+            V = self.value(v)
+
+            # Advantages Calc
+            a = F.relu(self.fc_advantages(x))
+            A = self.advantages(a)
+
+            # Calculate Q
+            Q = V + A - torch.mean(A, dim=1, keepdim=True)
+        else: 
+            Q = self.fc2(x) 
+        return Q
     
 if __name__ == '__main__':
     state_dim = 12 
